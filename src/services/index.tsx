2d4d90adb111d46122f4f60a6139a528e634b2f8
emreesrc <emreesrc@gmail.com>
2022-09-03 17:39:15 +0300

    Fix

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -3,7 +3,7 @@ import axios from "axios"
 export const fetchData = async ({ country }) => {
   const data = axios
     .get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=0861a5029ae242c98d1f8edcbf54215c`
+      `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${process.env.REACT_APP_API_KEY}`
     )
     .then((res) => res.data)
   return data

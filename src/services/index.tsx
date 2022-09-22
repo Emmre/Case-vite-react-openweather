e1e9cfddb8d8d07899ffc63d2a0d67bd97bf411a
Emre <47356515+Emmre@users.noreply.github.com>
2022-09-22 10:14:24 +0300

    Update index.tsx

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,9 +1,11 @@
 import axios from "axios"
 
+const REACT_APP_API_KEY = "fb9e237ca55ec441e335dd925bae5de2"
+
 export const fetchData = async ({ country }) => {
   const data = axios
     .get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${process.env.REACT_APP_API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${REACT_APP_API_KEY}`
     )
     .then((res) => res.data)
   return data
